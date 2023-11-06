51cf4da0367a5f6aa50e4497ed0df4f3a5b77444
Harshavardhan1617 <harshavardhan1617@gmail.com>
2023-11-06 20:17:10 +0530

    added template to user-search component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './services/api.service';
+import { UserSearchComponent } from './user-search/user-search.component';
 
 @Component({
   selector: 'app-root',
+  // imports: [
+  //   UserSearchComponent
+  // ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
